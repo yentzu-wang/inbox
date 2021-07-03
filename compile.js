15711d83829a53638257a65e6d999fea2977876f
Frank <frank7300228@gmail.com>
2021-07-03 16:00:29 +0800

    Finish 59

--- compile.js
+++ compile.js
@@ -1,8 +1,8 @@
-const path = require("path");
-const fs = require("fs");
-const solc = require("solc");
+const path = require("path")
+const fs = require("fs")
+const solc = require("solc")
 
-const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
-const source = fs.readFileSync(inboxPath, "utf8");
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol")
+const source = fs.readFileSync(inboxPath, "utf8")
 
-module.exports = solc.compile(source, 1).contracts[":Inbox"];
+module.exports = solc.compile(source, 1).contracts[":Inbox"]
